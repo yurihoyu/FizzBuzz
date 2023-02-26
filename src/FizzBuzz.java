d4a9332b83b087415dea871cfd9b52fcf2dad4f6
YasukoDmm <https://github.com/yurihoyu/eclipse-test.git>
2023-02-26 18:39:15 +0900

    課題提出

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -4,6 +4,19 @@ public class FizzBuzz {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
+		for(int i = 1; i <= 100; i++) {
+			if(i%3 == 0) {
+				System.out.println("Fizz");
+				if(i%5 == 0) {
+					System.out.println("FizzBuzz");
+				}
+			}else if(i%5 == 0) {
+				System.out.println("Buzz");
+			}else {
+				System.out.println(i);
+			}
+		}
+
 	}
 
 }
